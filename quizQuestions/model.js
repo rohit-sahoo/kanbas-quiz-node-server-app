3ac7712f23aec3b4d8d50eb51e5311cfbcdc91d9
Rohit Sahoo <50145744+rohit-sahoo@users.noreply.github.com>
2024-04-23 17:10:31 -0400

    Deleted files not required

--- quizQuestions/model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import { quizQuestionSchema } from "./schema.js";
-
-export const quizQuestionsModel = mongoose.model(
-  "QuizQuestionsModel",
-  quizQuestionSchema
-);
--- quizzes/model.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-import quizSchema from "./schema.js";
-
-export const quizzesModel = mongoose.model("QuizzesModel", quizSchema);
